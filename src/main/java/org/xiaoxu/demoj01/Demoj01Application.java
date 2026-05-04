66f7ed179d6b29c0f4a0fb25f63f54f55a0e02ab
QuentinXu <tingqq7z@gmail.com>
2026-05-04 18:00:31 +0800

    first commit

---
+++ src/main/java/org/xiaoxu/demoj01/Demoj01Application.java
@@ -0,0 +1,13 @@
+package org.xiaoxu.demoj01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoj01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demoj01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/xiaoxu/demoj01/Demoj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xiaoxu.demoj01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoj01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
